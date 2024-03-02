942480904a5924b098b115b7b954dad3dede48ea
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-03-02 21:57:38 +0530

    final2

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import styles from './Flag.module.css'
 
-export default function App() {
-  const [countries, setCountries] = useState([]);
+export default function Flag (){
+    const [countries,setCountries] = useState([]);
 
-  useEffect(() => {
-      fetch("https://restcountries.com/v2/all")
-        .then((res) => res.json())
-        .then((data) => setCountries(data))
-        . catch ((err) => {
-      console.error("Error fetching countries:", err);
-    })
-  }, []);
+    useEffect(()=>{
+        fetch("https://restcountries.com/v3.1/all")
+        .then((res)=>res.json())
+        .then((data)=>setCountries(data))
+        .catch((err)=>console.error("error from data:",err))
+    },[])
 
-  return (
-    <div className="App">
-      {countries.map((country) => (
-        <div className="countrys" key={country.name}>
-          <img src={country.flags.png} alt={country.name} />
-          <h6>{country.name}</h6>
+    return (
+        <div className={styles.main}>
+    {countries.map((country) => (
+        <div key={country.cca3} className={styles.wrapper}>
+            <img 
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+                className={styles.imgFlag} />
+            <h2 className={styles.heading}>{country.name.common}</h2>    
         </div>
-      ))}
-    </div>
-  );
+    ))}
+</div>
+
+    )
 }
